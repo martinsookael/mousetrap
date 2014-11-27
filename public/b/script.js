89b93ce7086b5af1d4f688865954f1d8eee6879a
Martin Sookael <m@451.ee>
2014-11-27 22:43:39 +0200

    hide cover on mousemove and click

--- public/b/script.js
+++ public/b/script.js
@@ -1,4 +1,6 @@
 
+var is = true;
+
 $(document).ready(function() {
 
   cl("mousemove, space and click are logged to console");
@@ -10,10 +12,45 @@ $(document).ready(function() {
   });
 
   $(document).mousemove(function(event){
+    showDialog();
+
+    if(is === true) {
+      hideDialog();
+      is = false;
+    }
+
     var msg = "Handler for .mousemove() called at ";
     msg += event.pageX + ", " + event.pageY;
     cl(msg);
   });
+
+  $(document).click(function(event){
+    showDialog();
+
+    if(is === true) {
+      hideDialog();
+      is = false;
+    }
+
+    var msg = "Click at ";
+    msg += event.pageX + ", " + event.pageY;
+    cl(msg);
+  });
+
+
+
+  function hideDialog() {
+    setTimeout(function(){
+      $("#dialog").fadeOut().addClass("hidden");
+      $("#cover").css("z-index", "2");
+      is = true;
+    }, 3000);
+  }
+
+  function showDialog() {
+    $("#dialog").removeClass("hidden").fadeIn();
+    $("#cover").css("z-index", "0");
+  }
 
   Mousetrap.bind('space', function() {
     cl('space');
@@ -22,6 +59,21 @@ $(document).ready(function() {
   $("#cover").on('click', function (){
     cl("click");
   })
+
+  $("#dialog").on('click', function() {
+    cl("dialog");
+  })
+
+  $("#b1").on('click', function() {
+    cl("button 1");
+  })
+
+  $("#b2").on('click', function() {
+    cl("button 2");
+  })
+
+
+
 
 });
 
